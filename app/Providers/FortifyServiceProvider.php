d237143b1b6b2b659ffa18d95b036db6b12465b9
Herve Desktop windows 10 <lova_67@live.fr>
2020-10-22 12:18:25 +0300

    Retrieve a fortify authenticate

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -36,30 +36,17 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function () {
             return view('auth.login');
         });
-    
-        Fortify::authenticateUsing(function (Request $request) {
-            $user = User::where('email', $request->email)->first();
-    
-            if ($user &&
-                Hash::check($request->password, $user->password)) {
-                return $user;
-            }
-        });
-    
+        
         Fortify::registerView(function () {
             return view('auth.register');
         });
-    
+        
         Fortify::requestPasswordResetLinkView(function () {
-            return view('auth.passwords.forgot');
+            return view('auth.forgot-password');
         });
-    
-        Fortify::resetPasswordView(function ($request) {
-            return view('auth.passwords.reset', ['request' => $request]);
-        });
-    
-        Fortify::verifyEmailView(function () {
-            return view('auth.verify');
+        
+        Fortify::resetPasswordView(function () {
+            return view('auth.reset-password');
         });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('home', 'home')->name('home');
